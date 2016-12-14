3848b7bac595e4973879c5a5185344ca8893553e
Jan Ivanov <jan.ivanov@emailpro.xyz>
2016-12-14 12:01:54 +0200

    day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -1,32 +1,37 @@
 with open('day3_input.txt') as f:
     triangles = f.readlines()
 
-i = 0
-p = 0
 
+t = 0
 for triangle in triangles:
     triangle = triangle.strip().split();
     triangle[0] = int(triangle[0])
     triangle[1] = int(triangle[1])
     triangle[2] = int(triangle[2])
+    triangles[t] = triangle
+    t = t + 1
 
-    if (triangle[0] + triangle[1] > triangle[2]) and (triangle[1] + triangle[2] > triangle[0]) and (triangle[2] + triangle[0] > triangle[1]):
-        p=p+1
-
-    if triangle[0] + triangle[1] < triangle[2]:
-#        print "%d + %d < %d" % (triangle[0], triangle[1], triangle[2])
+t = 0
+for triangle in triangles:
+    if triangle[0] + triangle[1] <= triangle[2]:
         continue
-    if triangle[1] + triangle[2] < triangle[0]:
-#        print "%d + %d < %d" % (triangle[1], triangle[2], triangle[0])
+    if triangle[1] + triangle[2] <= triangle[0]:
         continue
-    if triangle[2] + triangle[0] < triangle[1]:
-#        print "%d + %d < %d" % (triangle[2], triangle[0], triangle[1])
+    if triangle[2] + triangle[0] <= triangle[1]:
         continue
+    t = t + 1
 
+print "Part 1:",t
 
+t = 0
+for i in range(0,len(triangles),3):
+    if (triangles[i][0] + triangles[i+1][0] > triangles[i+2][0]) and (triangles[i+1][0] + triangles[i+2][0] > triangles[i][0]) and (triangles[i+2][0] + triangles[i][0] > triangles[i+1][0]):
+        t=t+1
 
-    i = i + 1
-#    print i
+    if (triangles[i][1] + triangles[i+1][1] > triangles[i+2][1]) and (triangles[i+1][1] + triangles[i+2][1] > triangles[i][1]) and (triangles[i+2][1] + triangles[i][1] > triangles[i+1][1]):
+        t=t+1
 
-print i
-print p
+    if (triangles[i][2] + triangles[i+1][2] > triangles[i+2][2]) and (triangles[i+1][2] + triangles[i+2][2] > triangles[i][2]) and (triangles[i+2][2] + triangles[i][2] > triangles[i+1][2]):
+        t=t+1
+
+print "Part 2:",t
